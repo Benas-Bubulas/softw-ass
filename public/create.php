7de95dc34a73163e840f3c9470e242f3c7b66ac1
Resistance205 <76964444+Resistance205@users.noreply.github.com>
2022-03-23 17:19:03 +0000

    Updated create

--- public/create.php
+++ public/create.php
@@ -1,23 +1,50 @@
-
-
-    <?php include "templates/header.php"; ?><h2>Add a user</h2> 
-    <style>
-      <?php include "css/style.css" ?>
-      </style>
-
-    <form method="post">
-      <label for="firstname">First Name</label>
-      <input type="text" name="firstname" id="firstname" required>
-      <label for="lastname">Last Name</label>
-      <input type="text" name="lastname" id="lastname" required>
-      <label for="email">Email Address</label>
-      <input type="email" name="email" id="email" required>
-      <label for="age">Age</label>
-      <input type="text" name="age" id="age">
-      <label for="location">Location</label>
-      <input type="text" name="location" id="location">
-      <input type="submit" name="submit" value="Submit">
-    </form>
-    <a href="index.php">Back to home</a>
-
-    <?php include "templates/footer.php"; ?>
+<?php
+/**
+ * Use an HTML form to create a new entry in the
+ * users table.
+ *
+ */
+if (isset($_POST['submit'])) {
+ require "../common.php";
+ try {
+require_once '../src/DBconnect.php';
+$new_user = array(
+ "firstname" => $_POST['firstname'],
+ "lastname" => $_POST['lastname'],
+ "email" => $_POST['email'],
+ "age" => $_POST['age'],
+ "location" => $_POST['location']
+);
+$sql = sprintf(
+ "INSERT INTO %s (%s) values (%s)",
+ "users",
+ implode(", ", array_keys($new_user)),
+ ":" . implode(", :", array_keys($new_user))
+);
+$statement = $connection->prepare($sql);
+$statement->execute($new_user);
+ } catch(PDOException $error) {
+ echo $sql . "<br>" . $error->getMessage();
+ }
+}
+?>
+<?php require "../templates/header.php"; ?>
+<?php if (isset($_POST['submit']) && $statement) { ?>
+ <?php echo escape($_POST['firstname']); ?> successfully added.
+<?php } ?>
+<h2>Add a user</h2>
+ <form method="post">
+ <label for="firstname">First Name</label>
+ <input type="text" name="firstname" id="firstname">
+ <label for="lastname">Last Name</label>
+ <input type="text" name="lastname" id="lastname">
+ <label for="email">Email Address</label>
+ <input type="text" name="email" id="email">
+ <label for="age">Age</label>
+ <input type="text" name="age" id="age">
+ <label for="location">Location</label>
+ <input type="text" name="location" id="location">
+ <input type="submit" name="submit" value="Submit">
+ </form>
+ <a href="index.php">Back to home</a>
+<?php include "../templates/footer.php"; ?>
