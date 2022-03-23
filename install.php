6766bcfb46e82ad110ed4e2f6681495f8ff06725
Resistance205 <76964444+Resistance205@users.noreply.github.com>
2022-03-23 16:47:43 +0000

    config and install
    Added the basic code for config and install

---
+++ install.php
@@ -0,0 +1,7 @@
+require "config.php";
+
+$connection = new PDO($connection = new PDO("mysql:host=$host", $username, $password, $options);
+ array(
+ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+ );
+);
