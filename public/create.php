50624689c8e57ad434abc84d776e9b35f2126d9a
nezinauuu <b00133471@mytudublin.ie>
2022-03-24 11:55:05 +0000

    Created DBconnect.php
    also made changes to create.php

--- public/create.php
+++ public/create.php
@@ -5,28 +5,33 @@
  *
  */
 if (isset($_POST['submit'])) {
- require "../common.php";
- try {
-require_once '../src/DBconnect.php';
-$new_user = array(
- "firstname" => $_POST['firstname'],
- "lastname" => $_POST['lastname'],
- "email" => $_POST['email'],
- "age" => $_POST['age'],
- "location" => $_POST['location']
-);
-$sql = sprintf(
- "INSERT INTO %s (%s) values (%s)",
- "users",
- implode(", ", array_keys($new_user)),
- ":" . implode(", :", array_keys($new_user))
-);
-$statement = $connection->prepare($sql);
-$statement->execute($new_user);
- } catch(PDOException $error) {
- echo $sql . "<br>" . $error->getMessage();
- }
+    try {
+        require_once './src/DBconnect.php';
+
+        $new_user = array(
+            "firstname" => $_POST['firstname'],
+            "lastname" => $_POST['lastname'],
+            "email" => $_POST['email'],
+            "age" => $_POST['age'],
+            "location" => $_POST['location']
+           );
+           $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "users",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+           );
+           $statement = $connection->prepare($sql);
+           $statement->execute($new_user);
+
+    }   catch (PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
 }
+
+
+
+
 ?>
 <?php require "templates/header.php"; ?>
 <?php if (isset($_POST['submit']) && $statement) { ?>
