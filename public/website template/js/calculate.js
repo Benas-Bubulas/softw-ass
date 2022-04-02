6e57b765599503120473dbe028d005e99bc76292
Resistance205 <76964444+Resistance205@users.noreply.github.com>
2022-04-02 14:25:30 +0100

    Minor functionality change

--- public/website template/js/calculate.js
+++ public/website template/js/calculate.js
@@ -29,32 +29,32 @@
 function Multiply()
 {
         num1 = document.getElementById("firstNumber").value;
-        num2 = 50.00;
+        num2 = 10.00;
         var result = num1 * num2;
     document.getElementById("total").innerHTML = 'Total: €' + result;
     document.getElementById("number1").innerHTML = 'Quantity: ' + num1;
     
 
         num3 = document.getElementById("firstNumber2").value;
-        num4 = 40.00;
+        num4 = 15.00;
         var result2 = num3 * num4;
     document.getElementById("total2").innerHTML = 'Total: €' + result2;
     document.getElementById("number2").innerHTML = 'Quantity: ' + num3;
     
         num5 = document.getElementById("firstNumber3").value;
-        num6 = 30.00;
+        num6 = 10.00;
     var result3 = num5 * num6;
     document.getElementById("total3").innerHTML = 'Total: €' + result3;
     document.getElementById("number3").innerHTML = 'Quantity: ' + num5;
     
         num7 = document.getElementById("firstNumber4").value;
-        num8 = 80.00;
+        num8 = 12.00;
     var result4 = num7 * num8;
     document.getElementById("total4").innerHTML = 'Total: €' + result4;
     document.getElementById("number4").innerHTML = 'Quantity: ' + num7;
     
         num9 = document.getElementById("firstNumber5").value;
-        num10 = 50.00;
+        num10 = 5.00;
     var result5= num9 * num10;
     document.getElementById("total5").innerHTML = 'Total: €' + result5;
     document.getElementById("number5").innerHTML = 'Quantity: ' + num9;
