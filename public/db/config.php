739b90f023ed8978e712ba635873dfc54f8d20ff
nezinauuu <B00133471@mytudublin.ie>
2022-05-01 12:56:09 +0100

    Revert Website folder
    Brought back the styles.css and images along with some javascript.

--- public/db/config.php
+++ public/db/config.php
@@ -5,13 +5,12 @@
  */
 $host = "localhost";
 $username = "root";
-$password = "";
+$password = "Nezinau123321";
 $dbname = "test"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
  );
-
 
 $Username = "jimmy";
 $Password = "pass";
