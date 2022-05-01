d3e26b35620c7d939b3f44a3c5b859e493d89916
RossLalor <b00137935@mytudublin.ie>
2022-05-01 14:46:05 +0100

    Removed references to location and date
    Removed redundant entries in the update file

--- public/db/update.php
+++ public/db/update.php
@@ -32,8 +32,6 @@ try {
             <th>Last Name</th>
             <th>Email Address</th>
             <th>Age</th>
-            <th>Location</th>
-            <th>Date</th>
             <th>Edit</th>
         </tr>
     </thead>
@@ -45,8 +43,6 @@ try {
             <td><?php echo escape($row["lastname"]); ?></td>
             <td><?php echo escape($row["email"]); ?></td>
             <td><?php echo escape($row["age"]); ?></td>
-            <td><?php echo escape($row["location"]); ?></td>
-            <td><?php echo escape($row["date"]); ?> </td>
             <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
         </tr>
     <?php endforeach; ?>
